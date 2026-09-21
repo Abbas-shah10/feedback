cf6c43a28a2c8cca09ffb87906c3e5c2ab82963c
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-09-21 11:17:39 +0500

    feat: add endpoint to check username uniqueness

---
+++ app/(auth)/check-username-unique/route.ts
@@ -0,0 +1,23 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+  try {
+  } catch (error) {
+    console.error("Error checking username", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error checking username",
+      },
+      { status: 500 },
+    );
+  }
+}
